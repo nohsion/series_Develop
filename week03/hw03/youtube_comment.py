6ac549b4971b126d74ba0e062d585fbd11b82c52
Sion Noh <shtldhs123@ajou.ac.kr>
2021-05-01 17:01:35 +0900

    유투브 댓글 scraping

---
+++ week03/hw03/youtube_comment.py
@@ -0,0 +1,66 @@
+from googleapiclient.discovery import build
+
+api_key = 'AIzaSyAZQVjnqFomqKOpjPA7VhRmyJzftGrRbS8'
+
+
+def video_comments(video_id):
+    # empty list for storing reply
+    replies = []
+
+    # creating youtube resource object
+    youtube = build('youtube', 'v3',
+                    developerKey=api_key)
+
+    # retrieve youtube video results
+    video_response = youtube.commentThreads().list(
+        part='snippet,replies',
+        videoId=video_id
+    ).execute()
+
+    # iterate video response
+    while video_response:
+
+        # extracting required info
+        # from each result object
+        for item in video_response['items']:
+
+            # Extracting comments
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+
+            # counting number of reply of comment
+            replycount = item['snippet']['totalReplyCount']
+
+            # if reply is there
+            if replycount > 0:
+
+                # iterate through all reply
+                for reply in item['replies']['comments']:
+                    # Extract reply
+                    reply = reply['snippet']['textDisplay']
+
+                    # Store reply is list
+                    replies.append(reply)
+
+            # print comment with list of reply
+            print(comment, replies, end='\n\n')
+
+            # empty reply list
+            replies = []
+
+        # Again repeat
+        # if 'nextPageToken' in video_response:
+        #     video_response = youtube.commentThreads().list(
+        #         part='snippet,replies',
+        #         videoId=video_id
+        #     ).execute()
+        # else:
+        #     break
+
+
+# Enter video id
+# video_id = input('Enter Video ID\n')
+video_url = input('Enter Youtube URL\n').split('v=')
+video_id = video_url[1]
+# Call function
+video_comments(video_id)
+print(video_id)
